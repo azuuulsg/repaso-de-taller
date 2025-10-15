c081da9f06d78649d5ee7c5dd79a8c6857e31fda
LeonelCifuentess <leocifuentes100907@gmail.com>
2025-10-15 16:37:26 -0300

    repaso actividad 2 clase 2

---
+++ main.js
@@ -0,0 +1,67 @@
+function ejercicio1(){
+
+
+    let cajamanzanas = 24;
+
+    cajacinco = 24 * 5
+    cajadiez = 24 * 10
+    cajaveinte = 24 * 20
+
+    console.log("la cantidad de manzanas es de "+cajacinco)
+    console.log("la cantidad de manzanas es de " +cajadiez)
+    console.log("la cantidad de manzanas es de " +cajaveinte)
+}
+
+function ejercici2(){
+
+
+    let gomitas = 20
+    let jugos = 25
+    let alfajores = 32
+}
+ 
+function ejercicio3(){
+
+
+    let precioAuto = parseInt(prompt("ingrese el precio del auto"))
+    let cuotas = 12;
+
+    if(precioAuto > 20000){
+        precioFinal = precioAuto * 0.85;
+
+    }
+
+    let valorcuota = precioFinal / cuotas;
+
+    console.log()
+}
+
+function ejercicio4(){
+    let contraseña = prompt("ingrese su contraseña");
+
+    let confirmar = prompt("confirme la contraseña ingresada");
+
+
+
+}
+
+function ej4(){
+
+    let contrasena = prompt("ingrese su contraseña")
+    let confirmar = prompt("vuelva a confirmar su contraseña")
+
+    if(contrasena == confirmar){
+        alert("contraseña correcta")
+
+    }else{
+        alert("contraseña incorrecta, vuelva a intentarlo")
+    }
+
+}
+
+function ej5(){
+    let montos = 5000
+    
+    let dinero = parseInt(prompt("ingrese la cantidad de dinero en cada venta "))
+
+}
