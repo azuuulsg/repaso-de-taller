9b894db579bae8da49bce2cecd6bf1e7e3e3bb31
azuuulsg <azulsgl4@gmail.com>
2025-12-03 13:16:32 -0300

    siguiendo trabajando con los ejercicios con walter

--- ejercicios.js
+++ ejercicios.js
@@ -1,6 +1,8 @@
+//const { cache } = require("react");
+
 document.write("ejemplo 1"+"<br>");
 
-class persona{
+class Persona{
     constructor(nombre, edad){
         this.nombre= nombre;
         this.edad = edad;
@@ -10,8 +12,8 @@ class persona{
     }
 }
 
-const persona1 = new persona("ana", 30);
-const persona2 = new persona("luis",25);
+const persona1 = new Persona("ana", 30);
+const persona2 = new Persona("luis",25);
 persona1.saludar(); //Hola, soy ana
 
 document.write( "<br>"+ "<br>"+ "ejemplo 2"+"<br>");
@@ -48,3 +50,63 @@ document.write("Notebook 150000"+"<br>");
 const prod2 = new Producto("teclado", 8000);
 document.write("teclado 8000"+"<br>");
 document.write("Precio mas IVA total"+prod1.calcularIVA());
+
+
+document.write("<br>"+ "objeto literal para representar una persona"+"<br>");
+
+const persona = {
+    nombre: "lucia",
+    edad: 28,
+    profesion: "ingeriero",
+    saludar(){
+        document.write(`hola, mi nombre es ${this.nombre}`);
+    }
+};
+persona.saludar(); //hola, me llamo lucia
+
+document.write("<br>"+"objeto literal para configurar una peticion o"+"<br>");
+const configuracion = {
+    URL: "https://api.nisitio.com",
+    metodo: "GET",
+    cache: false
+}
+document.write(configuracion.metodo);
+
+
+document.write("<br>"+"objeto literal como modulo de utilidades"+"<br>");
+const calculadora = {
+    sumar(a,b) { return a + b;},
+    restar(a , b) {return a-b;},
+    multiplicar(a,b) { return a * b;},
+}
+
+document.write(calculadora.multiplicar(3 , 5)) // 15
+
+document.write("ejemplo 1"+"<br>");
+let alumnos = [
+    {nombre: "ana", edad: 15},
+    {nombre: "luis", edad: 16}
+];
+
+function agregarAlumno(nombre, edad) {
+    alumnos.push({nombre, edad});
+
+}
+
+agregarAlumno("carla", 17);
+
+//mostrar contenido
+console.log(alumnos);
+
+
+document.write("<br>"+"ejemplo 2"+"<br>");
+let bibloteca = [];
+
+function agregarlibro(titulo, autor){
+    bibloteca.push({ titulo, autor});
+}
+
+agregarlibro("el princiíto", "a. de saint exupery");
+agregarlibro("1984", "george orwell");
+
+//hacer una app que me permita ingresar datos de un producto (almenos 5) codigo, producto, nombre, precio
